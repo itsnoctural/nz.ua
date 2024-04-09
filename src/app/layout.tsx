9d0c2774e0cc5a184d4f962e953a7f5d989d9f5c
itsnoctural <nocturalthe@gmail.com>
2024-04-09 21:57:39 +0300

    added speed insights and analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
-import "./globals.css";
 import { eUkraine } from "@/assets/fonts/eUkraine";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./globals.css";
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={eUkraine.className}>
       <body>
         {children}
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   )
